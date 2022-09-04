67388cdac3272b0b3eda25597206761fb9cf82f9
Jennifer Rodriguez <106123070+Jensr1998@users.noreply.github.com>
2022-09-03 19:13:16 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import React from 'react';
+import MovieList from './components/MovieList';
+import { storage } from './components/Storage';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const useState = React.useState
+
+function App() {
+  const [movies, setMovies] = useState(storage);
+
+  const addReview = (id, rating, comment) => {
+    const movie = movies.find(movie => movie.id === id);
+      movie.reviews.push(
+        {
+          user: `user$(Math.floor(Math.random() * 1000) + 100)`,
+          stars: rating,
+          comment: comment
+        }
+      );
+    setMovies([...movies, movie]);
+  }
+
+  return (
+      <>
+      <div className='container' />
+        <nav className="navbar navbar-light bg-warning">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1 text-dark">Jen's Movie Review</span>
+            </div>
+        </nav>
+      <div>
+      <MovieList movies={movies} addReview={addReview} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Storage.js
@@ -0,0 +1,50 @@
+import KikisPoster from "../images/Kiki's.png"
+import digimonPoster from "../images/digimon.jpg"
+import perfectbluePoster from "../images/perfectblue.jpg"
+
+export const storage = [
+    {
+        "id": 1,
+        "key": 1,
+        "title": "KiKi's Delivery Service",
+        "description": "It tells the story of Kiki, a young witch who moves to a new town and uses her flying ability to earn a living. ",
+        "released": 1989,
+        "image": KikisPoster,
+        "reviews": [
+         {
+            "stars": 5,
+            "comment": "Amazing!"
+          }
+        ]
+      },
+
+      {
+        "id": 2,
+        "key": 2,
+        "title": "Digimon Tri 1",
+        "description": "The film series is set six years after Digimon Adventure 02 and takes place in 2005. A mysterious anomaly is causing distortions in the Real World and Digimon are being plagued by a virus that turns them hostile.",
+        "released": 2015,
+        "image": digimonPoster,
+        "reviews": [
+         {
+            "stars": 5,
+            "comment": "Nostalgic!"
+          }
+        ]
+      },
+
+      {
+        "id": 3,
+        "key": 3,
+        "title": "Perfect Blue",
+        "description": "Mima Kirigoe, member of a J-pop idol group named CHAM, decides to leave the group to become a full-time actress. She gets stalked by an obsessive fan named Me-Mania, who is upset by her change from a clean-cut image.",
+        "released": 1997,
+        "image": perfectbluePoster,
+        "reviews": [
+         {
+            "stars": 5,
+            "comment": "Could not keep my eyes off the screen!"
+          }
+        ]
+      }
+]
